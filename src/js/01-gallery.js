d82143648d0e0e9b4885f55d8aee349c0cde7988
andrpavl <117930401+andrpavl@users.noreply.github.com>
2023-04-09 10:09:06 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector(".gallery");
+
+const markup = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+).join("");
+container.insertAdjacentHTML("beforeend", markup);
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+
+const saveInLocal = throttle(() => {
+  const inputData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputData));
+}, 500);
+ 
+emailInput.addEventListener('input', saveInLocal);
+messageInput.addEventListener('input', saveInLocal);
+
+const loadFromLocal = localStorage.getItem('feedback-form-state');
+if (loadFromLocal) {
+    JSON.parse(loadFromLocal) ?? [];
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const inputData = {
+        email: emailInput.value,    
+        message: messageInput.value,
+      };
+    console.log(inputData);
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+  });
